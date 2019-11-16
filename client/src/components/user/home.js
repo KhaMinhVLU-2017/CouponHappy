e15b526a1e45feb107a14205fb18b434303bd593
Nguyen <wesleynguyen1411@gmail.com>
2019-11-16 17:18:45 +0700

    Create new creact app

---
+++ client/src/components/user/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import { HomeHeader } from './home/HomeHeader';
+// import { HomeBody } from './home/HomeBody';
+
+export class Home extends Component {
+    componentDidMount() {
+        document.title = "Nguyen's blog";
+    }
+    render() {
+        document.getElementById('body').className = "index";
+        return (
+            <div>
+                {/* <HomeHeader/>
+                <HomeBody/> */}
+            </div>
+        );
+    }
+}
